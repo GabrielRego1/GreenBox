a1302e876fba7f885ce5d4de88cf56659512eb2d
GabrielRego1 <gabrielantonio280@gmail.com>
2022-08-28 15:53:47 -0300

    add: Notification Pattern

---
+++ src/GreenBox.Business/Interfaces/INotificador.cs
@@ -0,0 +1,10 @@
+﻿using GreenBox.Business.Notificacoes;
+namespace GreenBox.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
--- src/GreenBox.Data/Mappings/ColaboradorMap.cs
+++ src/GreenBox.Data/Mappings/ColaboradorMap.cs
@@ -1,12 +1,6 @@
 ﻿using GreenBox.Business.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace GreenBox.Data.Mappings
 {
     internal class ColaboradorMap : IEntityTypeConfiguration<Colaborador>
